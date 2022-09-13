bd8fa9fcf4222e39206f4fa60118393fc4adc6b7
Yash Ejjagiri <yse2@illiois.edu>
2022-09-13 16:49:18 -0500

    Added files

---
+++ config.py
@@ -0,0 +1,10 @@
+# standard config file
+
+BOT_TOKEN: str = "MTAxNjUyMDE5MDIzOTk4MTcxOA.GRVGz6.h3MyxS8Pt61bY3YaYZKSgJlGLFQ2Lmj6sn7Ss4"
+BOT_PREFIX = "-"
+
+STARTUP_MESSAGE = "Starting Bot..."
+STARTUP_COMPLETE_MESSAGE = "Startup Complete"
+
+HELP_PING_SHORT = "Pong"
+HELP_PING_LONG = "Test bot response status"
