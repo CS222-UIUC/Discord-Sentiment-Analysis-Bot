3c5967617b1da2a8b23dd393d265e2d6aae0db2b
shomali2 <shomali2@illinois.edu>
2022-09-30 20:23:31 -0500

    Organized repo & fixed pylint errors for discord bot

---
+++ discord_bot/general.py
@@ -0,0 +1,19 @@
+"""any defined command is hosted here"""
+
+import discord #pylint: disable=W0611
+from discord.ext import commands
+from discord.ext.commands import has_permissions #pylint: disable=W0611
+
+import discord_bot.config as config #pylint: disable=E0401
+
+class General(commands.Cog):
+    """Class with commands for bot"""
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command(name='ping', description=config.HELP_PING_LONG, help=config.HELP_PING_SHORT)
+    async def _ping(self, ctx):
+        await ctx.send("Pong")
+
+def setup(bot):
+    """"Function to setup bot"""
+    bot.add_cog(General(bot))
