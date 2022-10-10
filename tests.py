44e9df27c906e9544c627c0bb0d19c060f8dc1e1
mchew3611 <mnchew2@illinois.edu>
2022-10-09 21:44:27 -0500

    added basic tests to tests.py and .gitignore file

--- tests.py
+++ tests.py
@@ -1,2 +1,17 @@
-def test_always_passes():
-  assert True
+import pytest
+import discord.ext.test as dpytest
+from discord.ext import commands
+from discord import Intents
+
+@pytest.mark.asyncio
+async def test_bot():
+    bot = commands.Bot("/", intents=Intents(members=True))
+
+    @bot.command()
+    async def ping(ctx):
+        await ctx.send("pong")
+
+    dpytest.configure(bot)
+
+    await dpytest.message("/ping")
+    assert dpytest.verify().message().content("pong")
