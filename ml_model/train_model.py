364d14088c8a9ee8057f454e89f79851c3e0eaef
Sumeet Kulkarni <sumeetk2@illinois.edu>
2022-10-11 18:21:54 -0500

    Added training script and preprocessing methods (removing stopwords and lemmatizing)

---
+++ ml_model/train_model.py
@@ -0,0 +1,19 @@
+import nltk
+from nltk.tokenize import word_tokenize 
+from nltk.stem import WordNetLemmatizer
+
+def preprocess_text(input_text, stopwords):
+    preprocessed_input = word_tokenize(input_text)
+
+    for word in preprocessed_input:
+        if not word.isalpha() or word in stopwords:
+            preprocessed_input.remove(word)
+    
+    lemmatizer = WordNetLemmatizer()
+    for i in range(len(preprocessed_input)):
+        preprocessed_input[i] = preprocessed_input[i].lower()
+        preprocessed_input[i] = lemmatizer.lemmatize(preprocessed_input[i])
+    print(preprocessed_input)
+
+preprocess_text("The alumni meet the criteria.", ["the"])
+
