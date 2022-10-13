5d03756cedad51b4d78fc628b5f3f512a492aed0
Sumeet Kulkarni <sumeetk2@illinois.edu>
2022-10-13 17:18:50 -0500

    Fixed stopword removal issue with uppercase words

--- ml_model/train_model.py
+++ ml_model/train_model.py
@@ -4,14 +4,14 @@ from nltk.stem import WordNetLemmatizer
 
 def preprocess_text(input_text, stopwords):
     preprocessed_input = word_tokenize(input_text)
-
+    for i in range(len(preprocessed_input)):
+        preprocessed_input[i] = preprocessed_input[i].lower()
     for word in preprocessed_input:
         if not word.isalpha() or word in stopwords:
             preprocessed_input.remove(word)
     
     lemmatizer = WordNetLemmatizer()
     for i in range(len(preprocessed_input)):
-        preprocessed_input[i] = preprocessed_input[i].lower()
         preprocessed_input[i] = lemmatizer.lemmatize(preprocessed_input[i])
     print(preprocessed_input)
 
