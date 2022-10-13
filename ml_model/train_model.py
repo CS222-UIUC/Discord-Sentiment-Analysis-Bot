22873c68512c8581b93cde77d2645e62b2e72837
Sumeet Kulkarni <sumeetk2@illinois.edu>
2022-10-13 17:48:50 -0500

    Added NLTK Stopword corpus as global variable instead of parameter

--- ml_model/train_model.py
+++ ml_model/train_model.py
@@ -1,11 +1,13 @@
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize 
 from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+stopword_list = stopwords.words('english')
 
-def preprocess_text(input_text, stopwords):
-
-    for i in range(len(stopwords)):
-        stopwords[i] = stopwords[i].lower()
+def preprocess_text(input_text):
+    global stopword_list
+    for i in range(len(stopword_list)):
+        stopword_list[i] = stopword_list[i].lower()
 
     preprocessed_sentences = sent_tokenize(input_text)
     preprocessed_text = []
@@ -20,13 +22,14 @@ def preprocess_text(input_text, stopwords):
             tokenized_sentence[i] = tokenized_sentence[i].lower()
 
 
-            if tokenized_sentence[i].isalpha() and tokenized_sentence[i] not in stopwords:
+            if tokenized_sentence[i].isalpha() and tokenized_sentence[i] not in stopword_list:
 
                 tokenized_sentence[i] = lemmatizer.lemmatize(tokenized_sentence[i])
                 preprocessed_text.append(tokenized_sentence[i])
 
     print(preprocessed_text)
+    #print(stopword_list)
     return preprocessed_text
 
-preprocess_text("500 pennies makes $5.00 USD. $#%#$^!@#", ["The", "They", "be"])
+preprocess_text("You'll always be able to do it.")
 
