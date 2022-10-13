6f3eee3e89a31f8ee6b6682becb8348e21858b5c
Sumeet Kulkarni <sumeetk2@illinois.edu>
2022-10-13 17:36:32 -0500

    Fixed issue with non-alphabetical inputs making it to the preprocessed text. Also added support for input texts with multiple sentences

--- ml_model/train_model.py
+++ ml_model/train_model.py
@@ -1,19 +1,33 @@
 import nltk
-from nltk.tokenize import word_tokenize 
+from nltk.tokenize import sent_tokenize, word_tokenize 
 from nltk.stem import WordNetLemmatizer
 
 def preprocess_text(input_text, stopwords):
-    preprocessed_input = word_tokenize(input_text)
-    for i in range(len(preprocessed_input)):
-        preprocessed_input[i] = preprocessed_input[i].lower()
-    for word in preprocessed_input:
-        if not word.isalpha() or word in stopwords:
-            preprocessed_input.remove(word)
     
-    lemmatizer = WordNetLemmatizer()
-    for i in range(len(preprocessed_input)):
-        preprocessed_input[i] = lemmatizer.lemmatize(preprocessed_input[i])
-    print(preprocessed_input)
+    for i in range(len(stopwords)):
+        stopwords[i] = stopwords[i].lower()
 
-preprocess_text("The alumni meet the criteria.", ["the"])
+    preprocessed_sentences = sent_tokenize(input_text)
+    preprocessed_text = []
+    lemmatizer = WordNetLemmatizer()
+
+    for sentence in preprocessed_sentences:
+
+        tokenized_sentence = word_tokenize(sentence)
+
+        for i in range(len(tokenized_sentence)):
+
+            tokenized_sentence[i] = tokenized_sentence[i].lower()
+        
+        for i in range(len(tokenized_sentence)):
+
+            if tokenized_sentence[i].isalpha() and tokenized_sentence[i] not in stopwords:
+
+                tokenized_sentence[i] = lemmatizer.lemmatize(tokenized_sentence[i])
+                preprocessed_text.append(tokenized_sentence[i])
+
+    print(preprocessed_text)
+    return preprocessed_text
+
+preprocess_text("500 pennies makes $5.00 USD. $#%#$^!@#", ["The", "They", "be"])
 
