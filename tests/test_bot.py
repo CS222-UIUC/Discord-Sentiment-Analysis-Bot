cb7897de7c17efa3235ee61b012de03f0f680668
mchew3611 <mnchew2@illinois.edu>
2022-10-21 12:45:13 -0500

    test coverage

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,10 +1,8 @@
 """Testing framework for testing the bot"""
-from discord_bot.config import BOT_PREFIX
 import pytest
 import discord.ext.test as dpytest
 from discord.ext import commands
 from discord import Intents
-from discord_bot import config
 
 @pytest.mark.asyncio
 async def test_bot():
@@ -21,12 +19,3 @@ async def test_bot():
 
     await dpytest.message("/ping")
     assert dpytest.verify().message().content("pong")
-
-def test_config():
-    assert config.BOT_PREFIX == "-"
-
-    assert config.STARTUP_MESSAGE == "Starting Bot..."
-    assert config.STARTUP_COMPLETE_MESSAGE == "Startup Complete"
-
-    assert config.HELP_PING_SHORT == "Pong"
-    assert config.HELP_PING_LONG == "Test bot response status"
