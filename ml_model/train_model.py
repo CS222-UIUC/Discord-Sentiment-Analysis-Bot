efe5a705f088e6db1d732bc45f267a783c012819
Sumeet Kulkarni <sumeetk2@illinois.edu>
2022-10-23 17:30:42 -0500

    Added Logistic Regression model (~75% accuracy) and made changes to preprocessing (returns string with spaces between words now)

--- ml_model/train_model.py
+++ ml_model/train_model.py
@@ -2,34 +2,67 @@ import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize 
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
-stopword_list = stopwords.words('english')
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer 
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LogisticRegression 
+from sklearn.metrics import accuracy_score
+stopword_list = stopwords.words('english') 
+
 
 def preprocess_text(input_text):
     global stopword_list
-    for i in range(len(stopword_list)):
-        stopword_list[i] = stopword_list[i].lower()
 
-    preprocessed_sentences = sent_tokenize(input_text)
     preprocessed_text = []
     lemmatizer = WordNetLemmatizer()
 
-    for sentence in preprocessed_sentences:
 
-        tokenized_sentence = word_tokenize(sentence)
+    tokenized_sentence = word_tokenize(input_text)
+    
+    for i in range(len(tokenized_sentence)):
         
-        for i in range(len(tokenized_sentence)):
-            
-            tokenized_sentence[i] = tokenized_sentence[i].lower()
+        tokenized_sentence[i] = tokenized_sentence[i].lower()
 
 
-            if tokenized_sentence[i].isalpha() and tokenized_sentence[i] not in stopword_list:
+        if tokenized_sentence[i].isalpha() and tokenized_sentence[i] not in stopword_list:
 
-                tokenized_sentence[i] = lemmatizer.lemmatize(tokenized_sentence[i])
-                preprocessed_text.append(tokenized_sentence[i])
+            tokenized_sentence[i] = lemmatizer.lemmatize(tokenized_sentence[i])
+            preprocessed_text.append(tokenized_sentence[i])
 
-    print(preprocessed_text)
-    #print(stopword_list)
-    return preprocessed_text
+    output_text = ""
+    for word in preprocessed_text:
+        output_text = output_text + word + " "
+    output_text.strip()
+    #print(output_text)
+    return output_text
 
-preprocess_text("You'll always be able to do it.")
+def train_model():
+    train = pd.read_csv("twitter_training.csv", header=None)
+    test = pd.read_csv("twitter_validation.csv", header=None)
+    train.columns=['id','topic','label','text']
+    test.columns=['id','topic','label','text']
+    train["processed"] = [preprocess_text(str(x)) for x in train["text"]]
+    test["processed"] = [preprocess_text(str(x)) for x in test["text"]]
+
+    bag_of_words = CountVectorizer(
+        tokenizer=word_tokenize,
+        stop_words=stopword_list,
+        ngram_range=(1, 1) 
+    )
+
+    train_split, test_split = train_test_split(train, test_size=0.3, random_state=0)
+
+    train_text = bag_of_words.fit_transform(train_split.processed)
+    test_text = bag_of_words.transform(test_split.processed)
+
+    train_label = train_split["label"]
+    test_label = test_split["label"]
+
+    model = LogisticRegression(C=1, solver="liblinear",max_iter=1500)
+    model.fit(train_text, train_label)
+
+    predictions = model.predict(test_text)
+    print("Accuracy: ", accuracy_score(test_label, predictions) * 100)
+
+train_model()
 
