e0a037e44b2b98f0dfa0f9eb79ee77381de7f575
Sumeet Kulkarni <sumeetk2@illinois.edu>
2022-11-06 18:50:31 -0600

    Fixed pylint error

--- ml_model/train_model.py
+++ ml_model/train_model.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+#from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 stopword_list = stopwords.words('english')
 
