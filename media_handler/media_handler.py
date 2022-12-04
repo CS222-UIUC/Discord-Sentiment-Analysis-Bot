022b86c1dc0b98608d8fd6809dcc382ed33ebf72
Afif Shomali <shomali2@illinois.edu>
2022-12-04 17:20:26 -0600

    Update media_handler.py

--- media_handler/media_handler.py
+++ media_handler/media_handler.py
@@ -15,9 +15,9 @@ def process_message(message):
     message = msg_to_lower(message)
     message = remove_irrelevant_chars(message)
     message = process_using_nltk(message)
-    bow = CountVectorizer(tokenizer=word_tokenize,stop_words=stopwords_list)
-    vector = bow.fit_transform(message)
-    return vector
+    #bow = CountVectorizer(tokenizer=word_tokenize,stop_words=stopwords_list)
+    #vector = bow.fit_transform(message)
+    return message
 
 #Helpers
 def msg_to_lower(message):
